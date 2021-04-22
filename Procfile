8000bfd8f1e96bbb0ada2ace10558a136a3376a3
Thavasimani, Priyaa <Priyaa.Thavasimani@liverpool.ac.uk>
2021-04-22 22:48:34 +0100

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh setup.sh && streamlit run streamlit_example3.py
---
+++ streamlit_example3.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
+from urllib.request import urlopen
+import json
+import pandas as pd
+
+df = px.data.iris()
+
+fig= px.scatter(df, x="sepal_width", y = "sepal_length", color="species", size="petal_length", hover_data=['petal_width'])
+st.plotly_chart(fig)
+
+df= px.data.gapminder().query("continent == 'Oceania'")
+fig = px.line(df, x='year', y='lifeExp', color='country')
+st.plotly_chart(fig)
+
+fig = go.Figure(data=go.Scatter(
+    x=[1, 2, 3, 4],
+    y=[10, 11, 12, 13],
+    mode='markers',
+    marker=dict(size=[40, 60, 80, 100],
+                color=[0, 1, 2, 3])
+))
+st.plotly_chart(fig)
+
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
+
+df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
+                   dtype={"fips": str})
+
+fig = px.choropleth_mapbox(df, geojson=counties, locations='fips', color='unemp',
+                           color_continuous_scale="Viridis",
+                           range_color=(0, 12),
+                           mapbox_style="carto-positron",
+                           zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
+                           opacity=0.5,
+                           labels={'unemp':'unemployment rate'}
+                          )
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+st.plotly_chart(fig)
+
+data=[[1, 25, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, 5, 20]]
+fig = px.imshow(data,
+                labels=dict(x="Day of Week", y="Time of Day", color="Productivity"),
+                x=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+                y=['Morning', 'Afternoon', 'Evening']
+               )
+fig.update_xaxes(side="top")
+st.plotly_chart(fig)
